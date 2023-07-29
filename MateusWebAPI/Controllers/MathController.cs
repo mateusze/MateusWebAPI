f65c2719ebfc198cc33abb8b7c16763f163a255a
MateusJSB <mateu@WIN10P>
2023-07-29 11:02:07 -0300

    Add project files.

---
+++ MateusWebAPI/Controllers/MathController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MateusWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MathController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public UInt64 Index(int numberOfZeroes)
+        {
+            string myString = "1";
+
+            for (int i = 0; i < numberOfZeroes; i++)
+            {
+                myString += "0";
+            }
+
+            return Convert.ToUInt64(myString);
+        }
+    }
+}
